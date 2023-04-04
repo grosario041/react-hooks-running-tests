d11113716017ad73e605bff39741dd2d907aa820
Rosario Garcia <rosariogf041@gmail.com>
2023-04-03 23:01:20 -0400

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,10 @@ import React from "react";
 
 import Article from "../components/Article";
 
-test("displays the text 'please pass this test'", () => {
+test(("displays the text 'please pass this test'"), () => {
   render(<Article />);
+
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
